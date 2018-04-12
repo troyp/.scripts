d4ef375f7fd1b584af4e70ed7dfd7b3e525dc757
Troy Pracy <troyp7@gmail.com>
2018-04-12 22:13:08 +1000

    Update settings

--- script-env.sh
+++ script-env.sh
@@ -1,4 +1,6 @@
 # SETTINGS FOR SCRIPTS
 
 # onepiece_list_vol_chs
-export ONEPIECE_VOLUME_DATA=~/manga/raw/One\ Piece/.chapter-lists
+export ONEPIECE_VOLUME_DATA=~/manga/raw/One\ Piece/.manga-data
+export ONEPIECE_EPISODE_DATA=~/manga/raw/One\ Piece/.anime-data
+export NARUTO_EPISODE_DATA=~/manga/raw/Naruto/.anime-data
