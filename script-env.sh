746bade5aca6f5169c18f3630de48921b8d6e7fc
Troy Pracy <troyp7@gmail.com>
2020-04-08 19:25:46 +0000

    Update anime/manga data directories

--- script-env.sh
+++ script-env.sh
@@ -1,6 +1,6 @@
 # SETTINGS FOR SCRIPTS
 
 # onepiece_list_vol_chs
-export ONEPIECE_VOLUME_DATA=~/manga/raw/One\ Piece/.manga-data
-export ONEPIECE_EPISODE_DATA=~/manga/raw/One\ Piece/.anime-data
-export NARUTO_EPISODE_DATA=~/manga/raw/Naruto/.anime-data
+export ONEPIECE_VOLUME_DATA=~/Manga/One\ Piece/.manga-data
+export ONEPIECE_EPISODE_DATA=~/Manga/One\ Piece/.anime-data
+export NARUTO_EPISODE_DATA=~/Manga/Naruto/.anime-data
