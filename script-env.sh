deb00ebd924af00219fba1e977e49243801949e0
Troy Pracy <troyp7@gmail.com>
2020-09-13 11:04:13 +0000

    Update *_{EPISODE,VOLUME}_DATA environment variables

--- script-env.sh
+++ script-env.sh
@@ -1,6 +1,6 @@
 # SETTINGS FOR SCRIPTS
 
 # onepiece_list_vol_chs
-export ONEPIECE_VOLUME_DATA=~/Manga/One\ Piece/.manga-data
-export ONEPIECE_EPISODE_DATA=~/Manga/One\ Piece/.anime-data
-export NARUTO_EPISODE_DATA=~/Manga/Naruto/.anime-data
+export ONEPIECE_VOLUME_DATA=~/Manga/One\ Piece・ワンピース/.manga-data
+export ONEPIECE_EPISODE_DATA=~/Manga/One\ Piece・ワンピース/.anime-data
+export NARUTO_EPISODE_DATA=~/Manga/Naruto・ナルト/.anime-data
